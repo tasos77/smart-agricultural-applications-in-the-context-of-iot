498bc49820312fab06fa6ff2d644d8696b5bb914
tasos <lazarou.ts@gmail.com>
2023-08-09 18:42:23 +0300

    Add tabs on history page

--- WebApp/pages/history.vue
+++ WebApp/pages/history.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="text-h3">history</div>
+  <VTabs v-model="activeTab" grow :mandatory="true" show-arrows>
+    <VTab :value="tabs.length - index" v-for="(tab, index) in tabs" :key="index">{{ tab }}</VTab>
+  </VTabs>
 </template>
 
 <script setup lang="ts">
+  import moment from 'moment'
   definePageMeta({
     layout: 'main'
   })
+
+  const activeTab = ref(1)
+  const tabs = [
+    moment().subtract(5, 'days').format('MMM DD'),
+    moment().subtract(4, 'days').format('MMM DD'),
+    moment().subtract(3, 'days').format('MMM DD'),
+    moment().subtract(2, 'days').format('MMM DD'),
+    'Yesterday',
+    'Today'
+  ]
 </script>
 
 <style scoped></style>
