dcef14cd8caf6d28947f220f611384b2c62b7c30
Tasos <lazarou.ts@gmail.com>
2025-10-11 23:17:37 +0300

    Finished tb repo

--- TbApi/src/infra/repositories/thingsboardApi/api.ts
+++ TbApi/src/infra/repositories/thingsboardApi/api.ts
@@ -10,23 +10,10 @@ export interface ThingsboardApiApi {
   login: (username: string, password: string) => any
   logout: (accessToken: string) => any
   getUser: (accessToken: string) => any
-  activateUser: (
-    tenantToken: string,
-    activationInfo: { activateToken: string; password: string }
-  ) => any
+  activateUser: (tenantToken: string, activationInfo: { activateToken: string; password: string }) => any
   createCustomer: (tenantToken: string, email: string) => any
-  createUser: (
-    tenantToken: string,
-    customerId: string,
-    registrationInfo: { email: string; firstName: string; lastName: string }
-  ) => any
-  getTelemetryRange: (
-    tenantToken: string,
-    entityId: string,
-    startTs: number,
-    endTs: number,
-    keys = 'temperature,humidity,rain,soilMoisture,uv'
-  ) => any
+  createUser: (tenantToken: string, customerId: string, registrationInfo: { email: string; firstName: string; lastName: string }) => any
+  getTelemetryRange: (tenantToken: string, entityId: string, startTs: number, endTs: number, keys = 'temperature,humidity,rain,soilMoisture,uv') => any
   updateDeviceSharedAttribute: (tenantToken: string, deviceId: string, nextWatering: number) => any
 }
 
@@ -53,7 +40,8 @@ export const api = (deps: ThingsboardApiApiDeps): ThingsboardApiApi => {
   const logout = async (accessToken: string) => {
     try {
       logger.info(`Logging out`)
-      return await client.post(`/auth/logout`, {},
+      return await client.post(`/auth/logout`,
+        {},
         {
           headers: {
             'X-Authorization': `${accessToken}`
@@ -66,99 +54,131 @@ export const api = (deps: ThingsboardApiApiDeps): ThingsboardApiApi => {
     }
   }
 
-  const getUser = (accessToken: string) => {
-    return client.get('/auth/user', {
-      headers: {
-        'X-Authorization': `${accessToken}`
-      }
-    })
-  }
-
-  const activateUser = (token: string, activationInfo: { activateToken: string, password: string }) => {
-    console.log(activationInfo)
-    return client.post(
-      '/noauth/activate?sendActivationMail=false',
-      {
-        activateToken: activationInfo.activateToken,
-        password: activationInfo.password
-      },
-      {
+  const getUser = async (accessToken: string) => {
+    try {
+      logger.info(`Getting user`)
+      return await client.get('/auth/user', {
         headers: {
-          'X-Authorization': `Bearer ${token}`
+          'X-Authorization': `${accessToken}`
         }
-      }
-    )
+      })
+    } catch (e) {
+      logger.error(`Failed to get user, reason: ${e.message}`)
+      return e
+    }
   }
 
-  const createCustomer = (token: string, email: string) => {
-    return client.post(
-      '/customer',
-      {
-        title: email,
-        email
-      },
-      {
-        headers: {
-          'X-Authorization': `Bearer ${token}`
-        }
-      }
-    )
-  }
-
-  const createUser = (token: string, registrationInfo: { email: string, firstName: string, lastName: string }, customerId: string) => {
-    return client.post(
-      '/user',
-      {
-        customerId: {
-          id: customerId,
-          entityType: 'CUSTOMER'
+  const activateUser = async (token: string, activationInfo: { activateToken: string; password: string }) => {
+    try {
+      logger.info(`Activating user`)
+      return await client.post('/noauth/activate?sendActivationMail=false',
+        {
+          activateToken: activationInfo.activateToken,
+          password: activationInfo.password
         },
-        email: registrationInfo.email,
-        authority: 'CUSTOMER_USER',
-        firstName: registrationInfo.firstName,
-        lastName: registrationInfo.lastName
-      },
-      {
-        params: {
-          sendActivationMail: true
+        {
+          headers: {
+            'X-Authorization': `Bearer ${token}`
+          }
+        }
+      )
+    } catch (e) {
+      logger.error(`Failed to activate user, reason: ${e.message}`)
+      return e
+    }
+  }
+
+  const createCustomer = async (token: string, email: string) => {
+    try {
+      logger.info(`Creating customer`)
+      return await client.post('/customer',
+        {
+          title: email,
+          email
         },
+        {
+          headers: {
+            'X-Authorization': `Bearer ${token}`
+          }
+        }
+      )
+    } catch (e) {
+      logger.error(`Failed to create customer, reason: ${e.message}`)
+      return e
+    }
+  }
+
+  const createUser = async (token: string, registrationInfo: { email: string; firstName: string; lastName: string }, customerId: string) => {
+    try {
+      logger.info(`Creating user`)
+      return await client.post('/user',
+        {
+          customerId: {
+            id: customerId,
+            entityType: 'CUSTOMER'
+          },
+          email: registrationInfo.email,
+          authority: 'CUSTOMER_USER',
+          firstName: registrationInfo.firstName,
+          lastName: registrationInfo.lastName
+        },
+        {
+          params: {
+            sendActivationMail: true
+          },
+          headers: {
+            'X-Authorization': `Bearer ${token}`
+          }
+        }
+      )
+    } catch (e) {
+      logger.error(`Failed to create user, reason: ${e.message}`)
+      return e
+    }
+  }
+
+  const getTelemetryRange = async (token: string, entityId: string, startTs: number, endTs: number, keys = 'temperature,humidity,rain,soilMoisture,uv') => {
+    try {
+      logger.info(`Getting telemetry range`)
+      return await client.get(`/plugins/telemetry/DEVICE/${entityId}/values/timeseries?keys=${keys}&endTs=${endTs}&startTs=${startTs}&orderBy=ASC&limit=99999&agg=NONE`, {
         headers: {
           'X-Authorization': `Bearer ${token}`
         }
-      }
-    )
+      })
+    } catch (e) {
+      logger.error(`Failed to get telemetry range, reason: ${e.message}`)
+      return e
+    }
   }
 
-  const getTelemetryRange = (token: string, entityId: string, startTs: number, endTs: number, keys = 'temperature,humidity,rain,soilMoisture,uv') => {
-    return client.get(`/plugins/telemetry/DEVICE/${entityId}/values/timeseries?keys=${keys}&endTs=${endTs}&startTs=${startTs}&orderBy=ASC&limit=99999&agg=NONE`, {
-      headers: {
-        'X-Authorization': `Bearer ${token}`
-      }
-    })
-  }
-
-  const updateDeviceSharedAttr = (token: string, deviceId: string, nextWatering: number) => {
-    return client.post(
-      `plugins/telemetry/DEVICE/${deviceId}/attributes/SHARED_SCOPE`,
-      {
-        watering: nextWatering
-      },
-      {
-        headers: {
-          'X-Authorization': `Bearer ${token}`
+  const updateDeviceSharedAttribute = async (token: string, deviceId: string, nextWatering: number) => {
+    try {
+      logger.info(`Updating device shared attribute`)
+      return await client.post(
+        `plugins/telemetry/DEVICE/${deviceId}/attributes/SHARED_SCOPE`,
+        {
+          watering: nextWatering
+        },
+        {
+          headers: {
+            'X-Authorization': `Bearer ${token}`
+          }
         }
-      }
-    )
+      )
+    } catch (e) {
+      logger.error(`Failed to update device shared attribute, reason: ${e.message}`)
+      return e
+    }
   }
+
   return {
     login,
+    logout,
+    getUser,
     activateUser,
     createCustomer,
     createUser,
-    logout,
-    getUser,
     getTelemetryRange,
-    updateDeviceSharedAttr
+    updateDeviceSharedAttribute
   }
-
 }
--- TbApi/src/infra/repositories/thingsboardApi/repository.ts
+++ TbApi/src/infra/repositories/thingsboardApi/repository.ts
@@ -1,5 +1,6 @@
 import type { Config } from "../../../config/schema"
 import type { LoggerRepository } from "../../../core/repositories/logger/repository"
+import { api, type ThingsboardApiApi } from "./api"
 
 interface ThingsboardRepositoryDeps {
   config: Config
@@ -19,24 +20,41 @@ interface ThingsboardRepository {
 
 
 export const make = (deps: ThingsboardRepositoryDeps): ThingsboardRepository => {
-  const { logger } = deps
+  const { logger, config } = deps
 
+  const thingsboardApi: ThingsboardApiApi = api({ logger, thingsboardBaseUrl: config.thingsBoard.baseUrl })
 
-  const login = () => { }
+  const login = (username: string, password: string) => {
+    return thingsboardApi.login(username, password)
+  }
 
-  const logout = () => { }
+  const logout = (accessToken: string) => {
+    return thingsboardApi.logout(accessToken)
+  }
 
-  const getUser = () => { }
+  const getUser = (accessToken: string) => {
+    return thingsboardApi.getUser(accessToken)
+  }
 
-  const activateUser = () => { }
+  const activateUser = (tenantToken: string, activationInfo: { activateToken: string, password: string }) => {
+    return thingsboardApi.activateUser(tenantToken, activationInfo)
+  }
 
-  const createCustomer = () => { }
+  const createCustomer = (tenantToken: string, email: string) => {
+    return thingsboardApi.createCustomer(tenantToken, email)
+  }
 
-  const createUser = () => { }
+  const createUser = (tenantToken: string, customerId: string, registrationInfo: { email: string, firstName: string, lastName: string }) => {
+    return thingsboardApi.createUser(tenantToken, customerId, registrationInfo)
+  }
 
-  const getTelemetryRange = () => { }
+  const getTelemetryRange = (tenantToken: string, entityId: string, startTs: number, endTs: number, keys = 'temperature,humidity,rain,soilMoisture,uv') => {
+    return thingsboardApi.getTelemetryRange(tenantToken, entityId, startTs, endTs, keys)
+  }
 
-  const updateDeviceSharedAttribute = () => { }
+  const updateDeviceSharedAttribute = (tenantToken: string, deviceId: string, nextWatering: number) => {
+    return thingsboardApi.updateDeviceSharedAttribute(tenantToken, deviceId, nextWatering)
+  }
 
   return {
     login,
