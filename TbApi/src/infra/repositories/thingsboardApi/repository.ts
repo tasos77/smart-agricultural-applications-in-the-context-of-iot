17817d40c368398232c72d5fd8e95909b164e2c1
Tasos <lazarou.ts@gmail.com>
2025-10-11 03:00:41 +0300

    Working on tb repo

---
+++ TbApi/src/infra/repositories/thingsboardApi/repository.ts
@@ -0,0 +1,51 @@
+import type { Config } from "../../../config/schema"
+import type { LoggerRepository } from "../../../core/repositories/logger/repository"
+
+interface ThingsboardRepositoryDeps {
+  config: Config
+  logger: LoggerRepository
+}
+
+interface ThingsboardRepository {
+  login: (username: string, password: string) => any
+  logout: (accessToken: string) => void
+  getUser: (accessToken: string) => any
+  activateUser: (tenantToken: string, activationInfo: { activateToken: string, password: string }) => any
+  createCustomer: (tenantToken: string, email: string) => any
+  createUser: (tenantToken: string, customerId: string, registrationInfo: { email: string, firstName: string, lastName: string }) => any
+  getTelemetryRange: (tenantToken: string, entityId: string, startTs: number, endTs: number, keys = 'temperature,humidity,rain,soilMoisture,uv') => any
+  updateDeviceSharedAttribute: (tenantToken: string, deviceId: string, nextWatering: number) => any
+}
+
+
+export const make = (deps: ThingsboardRepositoryDeps): ThingsboardRepository => {
+  const { logger } = deps
+
+
+  const login = () => { }
+
+  const logout = () => { }
+
+  const getUser = () => { }
+
+  const activateUser = () => { }
+
+  const createCustomer = () => { }
+
+  const createUser = () => { }
+
+  const getTelemetryRange = () => { }
+
+  const updateDeviceSharedAttribute = () => { }
+
+  return {
+    login,
+    logout,
+    getUser,
+    activateUser,
+    createCustomer,
+    createUser,
+    getTelemetryRange,
+    updateDeviceSharedAttribute
+  }
+}
